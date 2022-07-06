36a93c95b1d2850c61a7a2154be58926ebce5e19
Aleksey Izmailov <izmailoff@gmail.com>
2022-07-06 20:03:06 +0800

    Add syntax rewrite options and apply them to code.

--- src/main/scala/com/izmailoff/mm/util/DbSerialization.scala
+++ src/main/scala/com/izmailoff/mm/util/DbSerialization.scala
@@ -6,16 +6,15 @@ import org.mongodb.scala.bson._
 
 import scala.util.Try
 
-object DbSerialization {
+object DbSerialization:
 
   val PAYLOAD_FIELD = MqttMongo.payloadField
 
   def serialize(payload: Array[Byte]): Document =
-    MqttMongo.serializationFormat match {
+    MqttMongo.serializationFormat match
       case JSON => serializeToJson(payload)
       case BINARY => serializeToBinary(payload)
       case STRING => serializeToString(payload)
-    }
 
   def serializeToJson(payload: Array[Byte]): Document =
     parseSafe(new String(payload))
@@ -34,4 +33,3 @@ object DbSerialization {
     } getOrElse {
         Document(PAYLOAD_FIELD -> msg)
     }
-}
--- src/main/scala/com/izmailoff/mm/util/StringUtils.scala
+++ src/main/scala/com/izmailoff/mm/util/StringUtils.scala
@@ -1,9 +1,8 @@
 package com.izmailoff.mm.util
 
-object StringUtils {
+object StringUtils:
 
   def emptyToNull(str: String): String =
-    if (str == null || str.trim.isEmpty) null
+    if str == null || str.trim.isEmpty then null
     else str
 
-}
